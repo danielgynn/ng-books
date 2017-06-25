68c9880b9ea393e020f2f0e6e22f20c5c776987c
danielgynn <danielgynn@hotmail.co.uk>
2017-06-25 12:07:19 +0100

    Serup basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms'
+import { FormsModule }   from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BookService } from './services/book.service';
@@ -15,7 +16,13 @@ import { BooksComponent } from './components/books/books.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'books',
+        component: BooksComponent
+      }
+    ])
   ],
   providers: [BookService],
   bootstrap: [AppComponent]
