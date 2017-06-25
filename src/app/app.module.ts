3147639eeb3e34f8750d2b860a17b6d616882c07
danielgynn <danielgynn@hotmail.co.uk>
2017-06-25 11:24:04 +0100

    Add book-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/book-detail/book-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+import Book from '../../models/book';
+
+@Component({
+  selector: 'book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+
+export class BookDetailComponent {
+  @Input() book: Book;
+}
